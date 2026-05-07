f31a89bb901067dd105b00cfa90523cf7ffdbbdd
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2026-05-07 16:26:06 +0300

    Python 3.15.0b1

--- Include/patchlevel.h
+++ Include/patchlevel.h
@@ -23,11 +23,11 @@
 #define PY_MAJOR_VERSION        3
 #define PY_MINOR_VERSION        15
 #define PY_MICRO_VERSION        0
-#define PY_RELEASE_LEVEL        PY_RELEASE_LEVEL_ALPHA
-#define PY_RELEASE_SERIAL       8
+#define PY_RELEASE_LEVEL        PY_RELEASE_LEVEL_BETA
+#define PY_RELEASE_SERIAL       1
 
 /* Version as a string */
-#define PY_VERSION              "3.15.0a8+"
+#define PY_VERSION              "3.15.0b1"
 /*--end constants--*/
 
 
